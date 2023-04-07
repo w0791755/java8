aefe7f5018c89a72b7ee75d66d0cc0fdd5898489
w0791755 <w0791755@myscc.ca>
2023-04-06 20:51:52 -0400

    Assignment8

--- src/app/hover-affect.directive.spec.ts
+++ src/app/hover-affect.directive.spec.ts
@@ -1,8 +1,9 @@
 import { HoverAffectDirective } from './hover-affect.directive';
+import { ElementRef } from '@angular/core';
 
 describe('HoverAffectDirective', () => {
   it('should create an instance', () => {
-    const directive = new HoverAffectDirective();
+    const directive = new HoverAffectDirective(new ElementRef(null));
     expect(directive).toBeTruthy();
   });
 });
--- src/helper-files/contentDb.ts
+++ src/helper-files/contentDb.ts
@@ -14,52 +14,35 @@ export const contentList = {
     generateContent: ():Content[]=>{
         contentListArr.push({
             ...generateContent(
-              1,['Avatar','Most popular aminated movie of all time','James Cameron']
+              1,['Rocket','Aminated movie','James']
             ),
-            ...{imgURL:'../assets/avatar.jpg'},
+            ...{imgURL:'../assets/ang.png'},
             ...{type:'Movie'},
             ...{tags:['Animated','Most Popular','Creative']}
         });
         contentListArr.push({
             ...generateContent(
-              2,['Friends',`Friends is a 90's Comedy TV show`,'David Crane and Marta Kauffman']
+              2,['GOT',`Famous TV show`,'David']
             ),
+            ...{tags :['famous']},
             ...{type:'Sitcom'},
-            ...{imgURL:'https://cdn.wallpapersafari.com/83/31/b59cLE.jpg'}
+            ...{imgURL:'../assets/ang.png'}
         });
         contentListArr.push({
             ...generateContent(
-              3,['Squid Game','Players in deep financial hardship, risk their lives to play a series of deadly Game','Hwang Dong-hyuk']
+              3,['Money Heist','Heist','Netflix']
             ),
-            ...{type:'Web-Series'}
+            ...{tags :['famous','Action']},
+            ...{type:'Web-Series'},
+            ...{imgURL:'../assets/ang.png'}
         });
           contentListArr.push({
             ...generateContent(
-              4,['3 Idiots',' Indian Hindi-language coming-of-age comedy-drama film written, edited and directed by Rajkumar Hirani','Rajkumar Hirani']
+              4,['Golmaal','Comedy-drama film','Rohit Shetty']
             ),
             ...{type:'Movie'},
-            ...{imgURL:'https://i.zoomtventertainment.com/story/Untitled_design_-_2020-05-01T135137.466.jpg'},
-            ...{tags:['Bollywood','Most Popular Hindispeaking Movie','Comedy','Based on book']}
-          });
-          contentListArr.push({
-            ...generateContent(
-              5,['A Beautiful Mind','A Beautiful Mind is a 2001 American biographical drama film directed by Ron Howard','Akiva Goldsman']
-            )
-          });
-          contentListArr.push({
-            ...generateContent(
-              6,['The Recruit','The Recruit is a 2003 American spy thriller film','Roger Donaldson']
-            )
-          });
-          contentListArr.push({
-            ...generateContent(
-              7,['The Shawshank Redemption','It is a 1994 American drama film','Frank Darabont']
-            )
-          });
-          contentListArr.push({
-            ...generateContent(
-              8,['The God Father','The Godfather is a 1972 American crime film','Francis Ford Coppola']
-            )
+            ...{imgURL:'../assets/ang.png'},
+            ...{tags:['Bollywood','Comedy']}
           });
       return contentListArr;
     },
